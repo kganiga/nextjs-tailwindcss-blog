01c6c2a5c02cd5055ddafef1041206559e167991
riv3rmak3r <khalil.b.ganiga@outlook.com>
2023-12-26 13:03:24 +0530

    comments not loading issue

--- next.config.js
+++ next.config.js
@@ -6,23 +6,25 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 const withPWA = require('next-pwa')
 module.exports = withPWA({
+  //...before
   pwa: {
     dest: 'public',
     register: true,
     skipWaiting: true,
   },
+  //...after
 })
 
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline'giscus.app www.googletagmanager.com www.google-analytics.com;
-  style-src 'self' 'unsafe-inline' *.googleapis.com cdn.jsdelivr.net;
-  frame-src youtube.com www.youtube.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is https://www.googletagmanager.com;
+  style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
   connect-src *;
-  font-src 'self' fonts.gstatic.com cdn.jsdelivr.net
+  font-src 'self' https://fonts.gstatic.com;;
+  frame-src giscus.app youtube.com www.youtube.com
 `
 
 const securityHeaders = [
