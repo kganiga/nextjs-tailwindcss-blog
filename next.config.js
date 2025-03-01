5c6b65ea4438195bdd6ad9476c89b07cbf331005
kganiga <khalil.b.ganiga@outlook.com>
2025-03-01 19:33:00 +0530

    restored old config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,21 @@
-const { withContentlayer } = require("next-contentlayer");
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development", // Disable PWA in dev mode
-});
+const { withContentlayer } = require('next-contentlayer')
 
-/** Security Headers */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+const withPWA = require('next-pwa')
+module.exports = withPWA({
+  //...before
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+  },
+  //...after
+})
+
+// You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
 default-src 'self';
 script-src 'self' www.google-analytics.com khalilganiga.disqus.com giscus.app analytics.umami.is https://www.googletagmanager.com va.vercel-scripts.com;
@@ -19,68 +25,85 @@ media-src *.s3.amazonaws.com;
 connect-src 'self' analytics.umami.is www.google-analytics.com;
 font-src 'self' https://fonts.gstatic.com;
 frame-src giscus.app www.youtube.com disqus.com;
-`;
+`
 
 const securityHeaders = [
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
   {
-    key: "Content-Security-Policy",
-    value: ContentSecurityPolicy.replace(/\n/g, ""),
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\n/g, ''),
   },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
   {
-    key: "Referrer-Policy",
-    value: "strict-origin-when-cross-origin",
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin',
   },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
   {
-    key: "X-Frame-Options",
-    value: "DENY",
+    key: 'X-Frame-Options',
+    value: 'DENY',
   },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
   {
-    key: "X-Content-Type-Options",
-    value: "nosniff",
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
   },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
   {
-    key: "X-DNS-Prefetch-Control",
-    value: "on",
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
   },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
   {
-    key: "Strict-Transport-Security",
-    value: "max-age=31536000; includeSubDomains",
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains',
   },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
   {
-    key: "Permissions-Policy",
-    value: "camera=(), microphone=(), geolocation=()",
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=()',
   },
-];
+]
 
-/** Final Next.js Configuration */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ["picsum.photos", "khalilganiga.in", "flickr.com", "freeimghost.net"],
-  },
-  eslint: {
-    dirs: ["app", "components", "layouts", "scripts"],
-  },
-  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: securityHeaders,
-      },
-    ];
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
+/**
+ * @type {import('next/dist/next-server/server/config').NextConfig}
+ **/
+module.exports = () => {
+  const plugins = [withContentlayer, withBundleAnalyzer]
+  return plugins.reduce((acc, next) => next(acc), {
+    reactStrictMode: true,
+    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+    eslint: {
+      dirs: ['app', 'components', 'layouts', 'scripts'],
+    },
+    images: {
+      domains: ['picsum.photos', 'khalilganiga.in', 'flickr.com', 'freeimghost.net'],
+    },
+    async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: securityHeaders,
+        },
+      ]
+    },
+    webpack: (config, options) => {
       config.module.rules.push({
         test: /\.svg$/,
-        use: ["@svgr/webpack"],
-      });
-    }
-    return config;
-  },
-};
+        use: ['@svgr/webpack'],
+      })
 
-/** Export Config with Plugins */
-module.exports = withContentlayer(withBundleAnalyzer(withPWA(nextConfig)));
+      return config
+    },
+  })
+}
+
+module.exports = {
+  swcMinify: true,
+}
+
+module.exports = {
+  images: {
+    domains: ['freeimghost.net'],
+  },
+}
